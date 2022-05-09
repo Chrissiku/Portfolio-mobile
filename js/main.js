7fa5a794086f1fb8fa694b7546c7b49d5050b05e
lomantech <87063237+lomantech@users.noreply.github.com>
2022-05-09 18:08:40 +0300

    add menu display on humberger click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-let mobileMenu = document.querySelector(".menu-icon");
-mobileMenu.addEventListener("click", () => {
-  if (mobileMenu.firstElementChild.classList.contains("fa-bars")) {
-    mobileMenu.firstElementChild.classList.replace("fa-bars", "fa-times");
+const sidebar = document.querySelector('header .menu ul');
+const mobileMenu = document.querySelector('.menu-icon');
+mobileMenu.addEventListener('click', () => {
+  if (mobileMenu.firstElementChild.classList.contains('fa-bars')) {
+    sidebar.style.display = 'block';
+    mobileMenu.firstElementChild.classList.replace('fa-bars', 'fa-times');
   } else {
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+    sidebar.style.display = 'none';
+    mobileMenu.firstElementChild.classList.replace('fa-times', 'fa-bars');
   }
 });
