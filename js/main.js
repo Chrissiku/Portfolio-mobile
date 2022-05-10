5513a5bd214fbf2388085e1222d882d9719b02f1
chrissiku <chrissiku5@gmail.com>
2022-05-10 12:22:19 +0200

    Fix linters errors for js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const sidebar = document.querySelector("header .menu ul");
-const mobileMenu = document.querySelector(".menu-icon");
+const sidebar = document.querySelector('header .menu ul');
+const mobileMenu = document.querySelector('.menu-icon');
 
-function hide_me() {
-  sidebar.addEventListener("click", () => {
-    sidebar.style.display = "none";
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+function hideMe() {
+  sidebar.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+    mobileMenu.firstElementChild.classList.replace('fa-times', 'fa-bars');
   });
 }
 
-mobileMenu.addEventListener("click", () => {
-  if (mobileMenu.firstElementChild.classList.contains("fa-bars")) {
-    sidebar.style.display = "block";
-    mobileMenu.firstElementChild.classList.replace("fa-bars", "fa-times");
-    hide_me();
+mobileMenu.addEventListener('click', () => {
+  if (mobileMenu.firstElementChild.classList.contains('fa-bars')) {
+    sidebar.style.display = 'block';
+    mobileMenu.firstElementChild.classList.replace('fa-bars', 'fa-times');
+    hideMe();
   } else {
-    sidebar.style.display = "none";
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+    sidebar.style.display = 'none';
+    mobileMenu.firstElementChild.classList.replace('fa-times', 'fa-bars');
   }
 });
