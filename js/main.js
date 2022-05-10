b7df47b99441c338f167fad105554bc36d9d1114
chrissiku <chrissiku5@gmail.com>
2022-05-10 12:13:39 +0200

    Add dropDown menu for mobile version

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const sidebar = document.querySelector('header .menu ul');
-const mobileMenu = document.querySelector('.menu-icon');
-mobileMenu.addEventListener('click', () => {
-  if (mobileMenu.firstElementChild.classList.contains('fa-bars')) {
-    sidebar.style.display = 'block';
-    mobileMenu.firstElementChild.classList.replace('fa-bars', 'fa-times');
+const sidebar = document.querySelector("header .menu ul");
+const mobileMenu = document.querySelector(".menu-icon");
+
+function hide_me() {
+  sidebar.addEventListener("click", () => {
+    sidebar.style.display = "none";
+    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+  });
+}
+
+mobileMenu.addEventListener("click", () => {
+  if (mobileMenu.firstElementChild.classList.contains("fa-bars")) {
+    sidebar.style.display = "block";
+    mobileMenu.firstElementChild.classList.replace("fa-bars", "fa-times");
+    hide_me();
   } else {
-    sidebar.style.display = 'none';
-    mobileMenu.firstElementChild.classList.replace('fa-times', 'fa-bars');
+    sidebar.style.display = "none";
+    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
   }
 });
