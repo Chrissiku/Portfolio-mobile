ffaeb2fe0e6726d61751a16b6ee542d5eaa2c18b
chrissiku <chrissiku5@gmail.com>
2022-05-11 17:06:42 +0200

    Design the popup window

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const sidebar = document.querySelector("header .menu ul");
 const mobileMenu = document.querySelector(".menu-icon");
 const aboutContent = document.querySelector("#about_content");
+const hideModalBtn = document.querySelector(".close_btn");
+const modal = document.querySelector(".popup");
+
+const showModalBtn = document.querySelector("#btn1");
 
 let work_post = [
   {
@@ -36,24 +40,6 @@ let work_post = [
   },
 ];
 
-function hideMe() {
-  sidebar.addEventListener("click", () => {
-    sidebar.style.display = "none";
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
-  });
-}
-
-mobileMenu.addEventListener("click", () => {
-  if (mobileMenu.firstElementChild.classList.contains("fa-bars")) {
-    sidebar.style.display = "block";
-    mobileMenu.firstElementChild.classList.replace("fa-bars", "fa-times");
-    hideMe();
-  } else {
-    sidebar.style.display = "none";
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
-  }
-});
-
 for (let i = 0; i < work_post.length; i++) {
   let fetch = document.querySelector("#about_content").innerHTML;
   if (i % 2 !== 0) {
@@ -67,10 +53,7 @@ for (let i = 0; i < work_post.length; i++) {
         <p>${work_post[i].content}</p>
         <div class="card-link">
           <ul>
-            <li><a href="">css</a></li>
-            <li><a href="">html</a></li>
-            <li><a href="">bootstrap</a></li>
-            <li><a href="">Ruby</a></li>
+          ${work_post[i].techology.map((j) => `<li>${j}</li>`)}
           </ul>
         </div>
         <button type="button" id="btn${i}" class="btn">
@@ -89,12 +72,9 @@ for (let i = 0; i < work_post.length; i++) {
     <h3>${work_post[i].title}</h3>
     <p>${work_post[i].content}</p>
     <div class="card-link">    
-      <ul>
-        <li><a href="">css</a></li>
-        <li><a href="">html</a></li>
-        <li><a href="">bootstrap</a></li>
-        <li><a href="">Ruby</a></li>
-      </ul>
+    <ul>
+    ${work_post[i].techology.map((j) => `<li>${j}</li>`)}
+    </ul>
     </div>
     <button type="button" id="btn${i}" class="btn">
       <span>See Project</span>
@@ -103,3 +83,41 @@ for (let i = 0; i < work_post.length; i++) {
 </div>` + fetch;
   }
 }
+
+function hideMenu() {
+  sidebar.addEventListener("click", () => {
+    sidebar.style.display = "none";
+    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+  });
+}
+
+function hideModal() {
+  if ((modal.style.display = "block")) {
+    modal.style.display = "none";
+  }
+}
+
+hideModalBtn.addEventListener("click", () => {
+  hideModal();
+});
+
+function showModal() {
+  if ((modal.style.display = "nome")) {
+    modal.style.display = "block";
+  }
+}
+
+showModalBtn.addEventListener("click", () => {
+  showModal();
+});
+
+mobileMenu.addEventListener("click", () => {
+  if (mobileMenu.firstElementChild.classList.contains("fa-bars")) {
+    sidebar.style.display = "block";
+    mobileMenu.firstElementChild.classList.replace("fa-bars", "fa-times");
+    hideMenu();
+  } else {
+    sidebar.style.display = "none";
+    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+  }
+});
