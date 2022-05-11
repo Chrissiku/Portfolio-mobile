956a194ae03606e1a65a45e34b5c40b04d75783b
chrissiku <chrissiku5@gmail.com>
2022-05-11 22:10:58 +0200

    Fix all linters errors and popup the detail popup

--- js/main.js
+++ js/main.js
@@ -1,123 +1,129 @@
-const sidebar = document.querySelector("header .menu ul");
-const mobileMenu = document.querySelector(".menu-icon");
-const aboutContent = document.querySelector("#about_content");
-const hideModalBtn = document.querySelector(".close_btn");
-const modal = document.querySelector(".popup");
+const sidebar = document.querySelector('header .menu ul');
+const mobileMenu = document.querySelector('.menu-icon');
+const aboutContent = document.querySelector('#about_content');
 
-const showModalBtn = document.querySelector("#btn1");
-
-let work_post = [
+const workPost = [
   {
-    title: "Multi-Post Stories 1",
-    image: "images/work_img.png",
+    title: 'Multi-Post Stories 1',
+    image: 'images/work_img.png',
     content:
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    techology: ["css", "html", "bootstrap", "Ruby"],
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    techology: ['css', 'html', 'bootstrap', 'Ruby'],
   },
 
   {
-    title: "Multi-Post Stories 2",
-    image: "images/work_img.png",
+    title: 'Multi-Post Stories 2',
+    image: 'images/work_img.png',
     content:
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    techology: ["css", "html", "bootstrap", "Ruby"],
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    techology: ['css', 'html', 'bootstrap', 'Ruby'],
   },
 
   {
-    title: "Multi-Post Stories 3",
-    image: "images/work_img.png",
+    title: 'Multi-Post Stories 3',
+    image: 'images/work_img.png',
     content:
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    techology: ["css", "html", "bootstrap", "Ruby"],
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    techology: ['css', 'html', 'bootstrap', 'Ruby'],
   },
 
   {
-    title: "Multi-Post Stories 4",
-    image: "images/work_img.png",
+    title: 'Multi-Post Stories 4',
+    image: 'images/work_img.png',
     content:
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    techology: ["css", "html", "bootstrap", "Ruby"],
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    techology: ['css', 'html', 'bootstrap', 'Ruby'],
   },
 ];
 
-for (let i = 0; i < work_post.length; i++) {
-  let fetch = document.querySelector("#about_content").innerHTML;
+for (let i = 0; i < workPost.length; i += 1) {
+  const fetch = document.querySelector('#about_content').innerHTML;
   if (i % 2 !== 0) {
-    aboutContent.innerHTML =
-      `<div class="card">
+    aboutContent.innerHTML = `<div class="card">
       <div class="card-image">
-        <img src="${work_post[i].image}" alt="Work image" />
+        <img src="${workPost[i].image}" alt="Work image" />
       </div>
       <div class="card-detail">
-        <h3>${work_post[i].title}</h3>
-        <p>${work_post[i].content}</p>
+        <h3>${workPost[i].title}</h3>
+        <p>${workPost[i].content}</p>
         <div class="card-link">
           <ul>
-          ${work_post[i].techology.map((j) => `<li>${j}</li>`)}
+          ${workPost[i].techology.map((j) => `<li>${j}</li>`)}
           </ul>
         </div>
-        <button type="button" id="btn${i}" class="btn">
+        <button type="button" id="btn-${i}" class="btn" data-open="popup">
           <span>See Project</span>
         </button>
       </div>
     </div>
-  ` + fetch;
+  ${fetch}`;
   } else {
-    aboutContent.innerHTML =
-      `<div class="card pair">
+    aboutContent.innerHTML = `<div class="card pair">
   <div class="card-image">
-    <img src="${work_post[i].image}" alt="Work image" />
+    <img src="${workPost[i].image}" alt="Work image" />
   </div>
   <div class="card-detail">
-    <h3>${work_post[i].title}</h3>
-    <p>${work_post[i].content}</p>
+    <h3>${workPost[i].title}</h3>
+    <p>${workPost[i].content}</p>
     <div class="card-link">    
     <ul>
-    ${work_post[i].techology.map((j) => `<li>${j}</li>`)}
+    ${workPost[i].techology.map((j) => `<li><a>${j}</a></li>`)}
     </ul>
     </div>
-    <button type="button" id="btn${i}" class="btn">
+    <button type="button" id="btn-${i}" class="btn" data-open="popup">
       <span>See Project</span>
     </button>
   </div>
-</div>` + fetch;
+</div>${fetch}`;
   }
 }
+
+// Show and hide Menu
 
 function hideMenu() {
-  sidebar.addEventListener("click", () => {
-    sidebar.style.display = "none";
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
+  sidebar.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+    mobileMenu.firstElementChild.classList.replace('fa-times', 'fa-bars');
   });
 }
+mobileMenu.addEventListener('click', () => {
+  if (mobileMenu.firstElementChild.classList.contains('fa-bars')) {
+    sidebar.style.display = 'block';
+    mobileMenu.firstElementChild.classList.replace('fa-bars', 'fa-times');
+    hideMenu();
+  } else {
+    sidebar.style.display = 'none';
+    mobileMenu.firstElementChild.classList.replace('fa-times', 'fa-bars');
+  }
+});
+
+// Show and Hide Detail popup
+
+const hideModalBtn = document.querySelector('.close_btn');
+const modal = document.querySelector('.popup');
 
 function hideModal() {
-  if ((modal.style.display = "block")) {
-    modal.style.display = "none";
-  }
+  modal.style.display = 'none';
 }
 
-hideModalBtn.addEventListener("click", () => {
+hideModalBtn.addEventListener('click', () => {
   hideModal();
 });
 
 function showModal() {
-  if ((modal.style.display = "nome")) {
-    modal.style.display = "block";
-  }
+  modal.style.display = 'block';
 }
 
-showModalBtn.addEventListener("click", () => {
-  showModal();
-});
+// Display specific post to the popup
 
-mobileMenu.addEventListener("click", () => {
-  if (mobileMenu.firstElementChild.classList.contains("fa-bars")) {
-    sidebar.style.display = "block";
-    mobileMenu.firstElementChild.classList.replace("fa-bars", "fa-times");
-    hideMenu();
-  } else {
-    sidebar.style.display = "none";
-    mobileMenu.firstElementChild.classList.replace("fa-times", "fa-bars");
-  }
-});
+for (let i = 0; i < workPost.length; i += 1) {
+  const showModalBtn = document.querySelectorAll(`[id=btn-${i}]`);
+  const postTitle = document.getElementById('post-title');
+  postTitle.textContent = workPost[i].title;
+
+  showModalBtn.forEach((btnShow) => {
+    btnShow.addEventListener('click', () => {
+      showModal();
+    });
+  });
+}
